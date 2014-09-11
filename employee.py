e8087d9e946b8461fcc7d19d654e905a7a9c79e1
VaticanCameos <chakrabortypritish@gmail.com>
2014-09-11 15:13:56 +0530

    Added a ID field and __setup__ function. Error messages and constraints defined in the setup function.

--- employee.py
+++ employee.py
@@ -9,7 +9,24 @@ __all__ = ['Employee']
 class Employee(ModelSQL, ModelView):
     "Employee"
     __name__ = "employee.employee"
+
+    ID = fields.Integer("ID", size=4, required=True, select=True)
     name = fields.Char("Name", required=True, select=True)
     phone = fields.Integer("Phone Number", size=10, required=True, select=True)
     designation = fields.Char("Designation", required=True, select=True)
     address = fields.Text("Address", required=True, select=True)
+
+    @classmethod
+    def __setup__(cls):
+        super(Employee, cls).__setup__()
+        cls._sql_constraints = [
+            ('PrimaryKey', 'PRIMARY KEY(ID)', 'pk_error'),
+            ('UniqueEmail', 'UNIQUE(email)', 'unique_error'),
+        ]
+        cls._sql_error_messages = {
+            'pk_error': 'This field is a primary key, it must\
+            be specified!',
+            'unique_error': 'This field must be unique!',
+        }
+
+
