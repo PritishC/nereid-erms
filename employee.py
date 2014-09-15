41a1c5f0fa7d1a2926b8606ed7ad82c059d9d602
Pritish Chakraborty <chakrabortypritish@gmail.com>
2014-09-15 17:23:26 +0530

    Added a name field and sql constraints.

--- employee.py
+++ employee.py
@@ -13,3 +13,12 @@ class Employee:
 
     designation = fields.Char("Designation", required=True, select=True)
     dob = fields.Date("Date of Birth", required=True)
+    name = fields.Char("Name", required=True, select=True)
+    
+    _sql_constraints = [('name_not_null', 'NOT NULL(name)', 
+                         'null_error'),
+                        ('dob_not_null', 'NOT NULL(dob)',
+                         'null_error')]
+    _sql_error_messages = {'uniq_error': 'This field must be unique.',
+                           'null_error': 'This field cannot be null.'}
+    
